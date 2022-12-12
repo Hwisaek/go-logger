189fa0177d51be5804f3d9ae3846f93ee80d07ec
Changmin Lee <50049254+Hwisaek@users.noreply.github.com>
2022-12-12 21:57:23 +0900

    로그 구조체 태그 키 추가

--- log/model.go
+++ log/model.go
@@ -7,21 +7,21 @@ import (
 )
 
 type data struct {
-	time    time.Time
-	level   int
-	file    string
-	line    int
-	message string
+	Time    time.Time `json:"time"`
+	Level   int       `json:"level"`
+	File    string    `json:"file"`
+	Line    int       `json:"line"`
+	Message string    `json:"message"`
 }
 
 func newData(level int, message string) data {
 	_, file, line, _ := runtime.Caller(1)
 	d := data{
-		time:    time.Now(),
-		level:   level,
-		file:    file,
-		line:    line,
-		message: message,
+		Time:    time.Now(),
+		Level:   level,
+		File:    file,
+		Line:    line,
+		Message: message,
 	}
 	return d
 }
